a5d927e4eb64cfbb5cc167e0ec1745fb0af71e57
SkippUser <84076538+SkippUser@users.noreply.github.com>
2022-05-15 22:01:59 +0300

    Старт от Хауди

---
+++ execute.py
@@ -0,0 +1,31 @@
+import config
+import datetime
+from num2t4ru import num2text
+import webbrowser
+import random
+
+def execute_cmd(cmd: str):
+    if cmd == 'help':
+        # help
+        text = "Я умею: ..."
+        text += "произносить время ..."
+        text += "рассказывать анекдоты ..."
+        text += "и открывать браузер"
+        return text
+    elif cmd == 'ctime':
+        # current time
+        now = datetime.datetime.now()
+        text = "Сейч+ас " + num2text(now.hour) + " " + num2text(now.minute)
+        return text
+
+    elif cmd == 'joke':
+        jokes = ['Как смеются программисты? ... ехе ехе ехе',
+                 'ЭсКьюЭль запрос заходит в бар, подходит к двум столам и спрашивает .. «м+ожно присоединиться?»',
+                 'Программист это машина для преобразования кофе в код']
+
+        return random.choice(jokes)
+
+    elif cmd == 'open_browser':
+        chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+        webbrowser.get(chrome_path).open("http://python.org")
+
---
+++ main.py
@@ -0,0 +1,50 @@
+import config
+import stt
+import tts
+from fuzzywuzzy import fuzz
+from execute import execute_cmd
+
+print(f"{config.VA_NAME} (v{config.VA_VER}) начал свою работу ...")
+
+
+def va_respond(voice: str):
+    print(voice)
+    if voice.startswith(config.VA_ALIAS):
+        # обращаются к ассистенту
+        cmd = recognize_cmd(filter_cmd(voice))
+
+        if cmd['cmd'] not in config.VA_CMD_LIST.keys():
+            tts.va_speak("Что?")
+        else:
+            text = execute_cmd(cmd['cmd'])
+            if text is not None:
+                tts.va_speak(text)
+
+
+def filter_cmd(raw_voice: str):
+    cmd = raw_voice
+
+    for x in config.VA_ALIAS:
+        cmd = cmd.replace(x, "").strip()
+
+    for x in config.VA_TBR:
+        cmd = cmd.replace(x, "").strip()
+
+    return cmd
+
+
+def recognize_cmd(cmd: str):
+    rc = {'cmd': '', 'percent': 0}
+    for c, v in config.VA_CMD_LIST.items():
+
+        for x in v:
+            vrt = fuzz.ratio(cmd, x)
+            if vrt > rc['percent']:
+                rc['cmd'] = c
+                rc['percent'] = vrt
+
+    return rc
+
+
+# начать прослушивание команд
+stt.va_listen(va_respond)
